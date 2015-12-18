480c06035122b2dc0031ef92346f771aec20bf4b
Nick <nickroberts57#gmail.com>
2015-12-17 19:18:02 -0800

    Added json file, added more UI

--- js/app.js
+++ js/app.js
@@ -3,30 +3,12 @@ var salesTaxApp = angular.module('salesTaxApp', []);
 
 
 
-salesTaxApp.controller('calcCtrl', function ($scope) {
+salesTaxApp.controller('calcCtrl', function ($scope, stateInfoService) {
     
-    $scope.states = [
-        {
-            'code': 'CA',
-            'name': 'California',
-            'tax': 0.08
-        },
-        {
-            'code': 'TX',
-            'name': 'Texas',
-            'tax': 0.07
-        },
-        {
-            'code': 'OR',
-            'name': 'Oregon',
-            'tax': 0.00
-        },
-        {
-            'code': 'AK',
-            'name': 'Alaska',
-            'tax': 0.03
-        },
-    ];
+    stateInfoService.states = stateInfoService.getStates(function(response) {
+        $scope.states = response.data;
+    });
+    
     $scope.currentState = -1
     $scope.price;
     
@@ -45,8 +27,12 @@ salesTaxApp.controller('calcCtrl', function ($scope) {
     };
     $scope.checkNumberValid = function(){
         return angular.isNumber($scope.price);
-    };
-    
+    };   
+});
+salesTaxApp.service('stateInfoService', function($http){
+   this.getStates = function(callback) {
+       $http.get('data/states.json').then(callback);
+   };
     
 });
 
