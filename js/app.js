e274ae03ba866399118ed4da0feec9238a204063
Nick <nickroberts57#gmail.com>
2015-12-17 19:39:26 -0800

    Added comments in js

--- js/app.js
+++ js/app.js
@@ -12,14 +12,14 @@ salesTaxApp.controller('calcCtrl', function ($scope, stateInfoService) {
     $scope.currentState = -1
     $scope.price;
     
-    $scope.select = function(state, index) {
+    $scope.select = function(state, index) { 
         $scope.selected = state;
         $scope.currentState = index;
     };
-    $scope.isActive = function(state) {
+    $scope.isActive = function(state) { //used to determine which state is highlighted
         return $scope.selected === state;
     };
-    $scope.checkIfSelected = function(){
+    $scope.checkIfSelected = function(){ //disallows price entry before state is selected
         if ($scope.currentState == -1){
             alert("Please Select a State First");
         }
@@ -29,7 +29,8 @@ salesTaxApp.controller('calcCtrl', function ($scope, stateInfoService) {
         return angular.isNumber($scope.price);
     };   
 });
-salesTaxApp.service('stateInfoService', function($http){
+
+salesTaxApp.service('stateInfoService', function($http){ //gets state information
    this.getStates = function(callback) {
        $http.get('data/states.json').then(callback);
    };
